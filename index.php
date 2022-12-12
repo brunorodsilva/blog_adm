cb8b9ebaa69bc8d20d6ca6c337fc0c9a61621087
brunorodsilva <brunord2014@hotmail.com>
2022-12-12 15:56:50 -0300

    add: projeto-login

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Administrador</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="container">
+    <div id="container-login">
+    <span id="title-login"><h1>Login</h1></span>
+    <div id="imagem-login">
+      <img src="imagens/user.png">
+    </div>
+      <form action="" method="">
+        <input type="text" placeholder="E-mail">
+        <input type="text" placeholder="Senha">
+        <span id="button-form"><button id="btn-login" type="submit">LOGIN</button></span>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
