50dffb76fbc6c097a26e038b81cf68f38457fe05
brunorodsilva <brunord2014@hotmail.com>
2022-12-13 15:55:47 -0300

    add: criação tela de cadastro - incompleta

--- cadastro.php
+++ cadastro.php
@@ -10,16 +10,19 @@
 <body>
   <div id="container">
     <div id="container-login">
-    <span id="title-login"><h1>Login</h1></span>
+    <span id="title-login"><h1>Cadastro</h1></span>
     <div id="imagem-login">
       <img src="imagens/user.png">
     </div>
       <form action="" method="">
+        <input type="text" placeholder="Nome">
+        <input type="text" placeholder="Sobrenome">
         <input type="text" placeholder="E-mail">
         <input type="text" placeholder="Senha">
+        <input type="text" placeholder="C">
         <span id="button-form"><button id="btn-login" type="submit">LOGIN</button></span>
       </form>
-      <span id="cadastro">Ainda não possui uma conta? <a href="cadastro.php">Cadastre-se</a></span>
+      <span id="cadastro">Já possui uma conta? <a href="index.php">Login</a></span>
     </div>
   </div>
 </body>
